1bd45c6dbb2144249012540722c8ec982d2684f4
mflo <annonnier@gmail.com>
2025-02-17 17:34:27 +0500

    bigger frame labes size

--- frame.py
+++ frame.py
@@ -14,9 +14,25 @@ class NODEHELPER_OT_set_frame_color(Operator):
         if active_tree:
             for node in active_tree.nodes:
                 if node.select and node.type == 'FRAME':
-                    # Set HSV color (0, 0, 0.3)
                     node.use_custom_color = True
-                    node.color = (0.3, 0.3, 0.3)  # RGB equivalent of HSV(0,0,0.3)
+                    node.color = (0.3, 0.3, 0.3)
+        
+        return {'FINISHED'}
+
+class NODEHELPER_OT_increase_label_size(Operator):
+    bl_idname = "nodehelper.increase_label_size"
+    bl_label = "Bigger Label Size"
+    bl_description = "Increase label size of selected frames by 10"
+    bl_options = {'REGISTER', 'UNDO'}
+
+    def execute(self, context):
+        space = context.space_data
+        active_tree = space.edit_tree or space.node_tree
+        
+        if active_tree:
+            for node in active_tree.nodes:
+                if node.select and node.type == 'FRAME':
+                    node.label_size += 10
         
         return {'FINISHED'}
 
@@ -38,17 +54,23 @@ class NODEHELPER_PT_frame(Panel):
         box = layout.box()
         box.label(text="Frame Operations")
         
-        # Make the button 1.5 times bigger
+        # Make the buttons 1.5 times bigger
+        row = box.row()
+        row.scale_y = 1.5
+        row.operator("nodehelper.increase_label_size", text="Bigger Label Size")
+        
         row = box.row()
         row.scale_y = 1.5
         row.operator("nodehelper.set_frame_color", text="Set Dark Frame Color")
 
 def register():
     bpy.utils.register_class(NODEHELPER_OT_set_frame_color)
+    bpy.utils.register_class(NODEHELPER_OT_increase_label_size)
     bpy.utils.register_class(NODEHELPER_PT_frame)
 
 def unregister():
     bpy.utils.unregister_class(NODEHELPER_PT_frame)
+    bpy.utils.unregister_class(NODEHELPER_OT_increase_label_size)
     bpy.utils.unregister_class(NODEHELPER_OT_set_frame_color)
 
 if __name__ == "__main__":
